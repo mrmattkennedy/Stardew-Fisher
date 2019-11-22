fb923a58a1da5710f91c07190adcfd1c25fdcfa5
Kennedy <matthew.kennedy1@stryker.com>
2019-11-22 09:10:24 -0500

    Initialized gym constructor

--- python/test.py
+++ python/test.py
@@ -1,3 +1,38 @@
-import gym
-import stardew_fisher
-env = gym.make('StardewFisher-v0')
+import numpy as np
+nA = 4
+nS = 7
+
+P = {s : {a : [] for a in range(nA)} for s in range(nS)}
+#print(type(P))
+
+
+MAPS = {
+    "4x4": [
+        "SFFF",
+        "FHFH",
+        "FFFH",
+        "HFFG"
+    ],
+    "8x8": [
+        "SFFFFFFF",
+        "FFFFFFFF",
+        "FFFHFFFF",
+        "FFFFFHFF",
+        "FFFHFFFF",
+        "FHHFFFHF",
+        "FHFFHFHF",
+        "FFFHFFFG"
+    ],
+}
+
+desc = MAPS["4x4"]
+print(desc)
+desc = desc = np.asarray(desc,dtype='c')
+print(desc)
+#isd: finds all the starting points.
+#For fishing: starting point is 1.0 for the poisition of the rod
+isd = np.array(desc == b'S').astype('float64').ravel()
+print(isd)
+isd /= isd.sum()
+print(isd)
+print(desc.shape)
