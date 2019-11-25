76b018d0d8e10a14554d4c301fb97e342dd32f2a
Kennedy <matthew.kennedy1@stryker.com>
2019-11-25 13:29:45 -0500

    Step function in progress, creating reward based on time and difference

--- python/stardew_fisher/envs/stardew_fisher_env.py
+++ python/stardew_fisher/envs/stardew_fisher_env.py
@@ -1,5 +1,7 @@
-import numpy as np
 import gym
+import math
+import time
+import numpy as np
 from gym import error, spaces
 from gym.utils import seeding
 from gym.envs.toy_text import discrete
@@ -20,24 +22,78 @@ class StardewFisherEnv(gym.Env):
     metadata = {'render.modes': ['human']}
     def __init__(self):
         self.observation_space = spaces.Tuple(tuple(spaces.Discrete(observations[key]) for key in observations))
-        self.action_space = spaces.Discrete(len(spaces))
+        self.action_space = spaces.Discrete(len(actions))
+        
+        self.bar_location = None
+        self.fish_location = None
+        self.catching_range = 10
+        self.catching = True
+        
+        self.last_time = None
+        self.current_time = None
+        self.time_elapsed = None
+        self.max_time = 8 #max time is 8 seconds to catch the fish
 
     def step(self, action):
+        """
+        Make sure action exists
+        Update timer
+        Get reward based on difference and time
+        """
         assert self.action_space.contains(action)
-        if action: #start hold
-            #TODO: Update bar location
-            
-    def _get_obs(self):
-        return (self.fish_location, self.bar_location)
-    
+        self._update_time()
+
+        done = False
+        if self.fish_location > 100 or self.fish_location < 0: #fish caught
+            done = True
+            reward = 1
+        else: #fish is somewhere
+            reward = self._get_rew()
+        
+        return self._get_obs(), reward, done, {}
+
     def reset(self):
         self.bar_location = 0
         self.fish_location = 0
         return self._get_obs()
+            
+    def _get_obs(self):
+        return (self.fish_location, self.bar_location)
+
+    def _start_timer(self):
+        return time.time()
+
+    #get time elapsed of catching fish
+    def _update_time(self):
+        self.current_time = self._start_timer()
+        
+        if self.last_time is not None and self.current_time is not None:            
+            self.time_elapsed = self._check_elapsed() + (self.current_time - self.last_time)
+            self.last_time = self.current_time
+        else:
+            self.last_time = time.time()
+
+    def _check_elapsed(self):
+        return ((lambda: 0, lambda: self.time_elapsed)[abs(self._get_difference()) <= self.catch_range]())
+
+    def _get_difference(self)
+        values = self._get_obs()
+        return values[1] - values[0]
+    
+    def _get_rew(self):
+        #diff max is 100?
+        #greater difference = lesser reward
+        #fix this
+        return -(self.elapsed_time * abs(self.bar_location - self.fish_location))
+    
 
 """
 Need:
 Determine if use models or actual game
 If models: get equations for change in bar
 If game: wait in step until next set of info comes in.
+
+For reward:
+If diff is within range, then diff * time
+If not, reward slowly from 0 -> -1
 """
--- python/test.py
+++ python/test.py
@@ -1,54 +1,26 @@
 import gym
+import time
 from gym import error, spaces
 import stardew_fisher
 import numpy as np
 
+def test():
+    return 3, 4
 """
-nA = 4
-nS = 7
-
-P = {s : {a : [] for a in range(nA)} for s in range(nS)}
-#for item in P:
-    #print(item, P[item])
-print(P[0][0])
+Flow:
 
 
-MAPS = {
-    "4x4": [
-        "SFFF",
-        "FHFH",
-        "FFFH",
-        "HFFG"
-    ],
-    "8x8": [
-        "SFFFFFFF",
-        "FFFFFFFF",
-        "FFFHFFFF",
-        "FFFFFHFF",
-        "FFFHFFFF",
-        "FHHFFFHF",
-        "FHFFHFHF",
-        "FFFHFFFG"
-    ],
-}
-
-desc = MAPS["4x4"]
-#print(desc)
-desc = desc = np.asarray(desc,dtype='c')
-#print(desc)
-#isd: finds all the starting points.
-#For fishing: starting point is 1.0 for the poisition of the rod
-isd = np.array(desc == b'S').astype('float64').ravel()
-#print(isd)
-isd /= isd.sum()
-#print(isd)
-#print(desc.shape)
+OpenCV aspect will continue to read where the positions of the fish and bar are.
+Send in to step function, and do the option with the
 """
-observations = {
-    "A" : 100,
-    "B" : 100}
+#observations = {
+    #"A" : 100,
+    #"B" : 100}
 #spaces_total = spaces.Tuple(tuple(spaces.Discrete(observations[key]) for key in observations))
 #print(spaces_total.sample())
 #print(t)
 #spaces.Discrete(i) for i in )
+t = None
+print(str(t is None))
+
 env = gym.make('StardewFisherEnv-v0')
